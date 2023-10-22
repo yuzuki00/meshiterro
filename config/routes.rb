511b756a726a7d5acb0fbc38c68cad5ca9d14718
yuzuki00 <144969224+yuzuki00@users.noreply.github.com>
2023-10-22 02:40:35 +0000

    [Finish] 4-14

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: "homes#top"
 
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit]
 
   get 'homes/about' => "homes#about", as: 'about'
 end
