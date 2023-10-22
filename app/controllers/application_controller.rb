f83a55c71e7787eaf1c7f1f78f71cb3dcef357f9
yuzuki00 <144969224+yuzuki00@users.noreply.github.com>
2023-10-22 02:02:48 +0000

    [Finish] 4-11

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    about_path
+    post_images_path
   end
-  
+
   def after_sign_out_path_for(resource)
     about_path
   end
