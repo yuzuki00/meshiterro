19ef0f7b6d2e46994ef4145ce6851d25c84f30f1
yuzuki00 <144969224+yuzuki00@users.noreply.github.com>
2023-10-25 07:08:05 +0000

    [Finish] 4-ほかのユーザーからのアクセスの制限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
+before_action :is_matching_login_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
-    @post_images = @user.post_image.page(params[:page])
+    @post_images = @user.post_images.page(params[:page])
   end
 
   def edit
@@ -14,7 +15,16 @@ class UsersController < ApplicationController
     redirect_to user_path(@user.id)
   end
 
+private
+
   def user_params
     params.require(:user).permit(:name, :profile_image)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
   end
 end
